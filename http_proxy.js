6b52e87574d2b4f7427cd1cc57941c4f7ad19f89
bartbutenaers <bart.butenaers@gmail.com>
2019-07-12 08:12:32 +0200

    Added basic settings

--- http_proxy.js
+++ http_proxy.js
@@ -18,29 +18,51 @@ module.exports = function(RED) {
     
     function ReverseProxyNode(config) {
         RED.nodes.createNode(this, config);
-        
+
         var node = this;
         
         node.proxyServer = httpProxy.createProxyServer({});
         
         node.proxyServer.on('error', function(err) {
-            console.log(err);
+            node.error(err);
         });
         
+        debugger;
+            
         var options = {
             target: config.url,
-            forward: config.url,
+            //forward: config.url,
             // Avoid UNABLE_TO_VERIFY_LEAF_SIGNATURE for redirects to a server with self signed certificates.
             // See https://github.com/http-party/node-http-proxy/issues/1083
-            secure: false,
+            secure: config.verifyCertificates,
+            followRedirects: config.followRedirects,
             // The path in the original request (to Node-RED) shouldn't be appended to the forwarded URL.
             // E.g. when somebody uses http://<node-red-host>:1880/somepath, then 'somepath' shouldn't be appended to target url.
-            ignorePath: true
+            ignorePath: true,
+            changeOrigin: config.changeOrigin,
+            preserveHeaderKeyCase: config.preserveHeaderKeyCase,
+            verifyCertificates: config.verifyCertificates,
+            followRedirects: config.followRedirects,
+            toProxy: config.toProxy,
+            // Keep the protocol (http or https) as specified in the target url
+            protocolRewrite: null,
+            xfwd: config.xfwd
+        }
+        
+        if ((config.incomingTimeout || 0) > 0) {
+            options.timeout = config.incomingTimeout;
+        }
+        
+        if ((config.outgoingTimeout || 0) > 0) {
+            options.proxyTimeout = config.outgoingTimeout;
+        }
+        
+        // The credentials are available in this node instance, not in the config !
+        if (node.credentials && node.credentials.user) {
+            options.auth = node.credentials.user + ":" + (node.credentials.password || "");
         }
         
         node.on("input", function(msg) {
-            
-            debugger;
             // Redirect the http(s) request to the specified URL, and write the response (from that host)
             node.proxyServer.web(msg.req, msg.res._res, options);
         });
@@ -50,5 +72,10 @@ module.exports = function(RED) {
         });
     }
 
-    RED.nodes.registerType("reverse-proxy", ReverseProxyNode);
+    RED.nodes.registerType("reverse-proxy", ReverseProxyNode,{
+        credentials: {
+            user: {type:"text"},
+            password: {type: "password"}
+        }
+    });
 }
